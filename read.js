0dd4e2501e096f71ccb816c1744fc27afd72bd2f
Angy Rincón <rinconmezangelica@gmail.com>
2023-05-23 10:45:11 -0600

    update with error message

--- read.js
+++ read.js
@@ -2,8 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 function leer(ruta, callback) {
-    fs.readFile(ruta, (err, data) =>{
-        console.log(data.toString());
+    fs.readFile(ruta, (err,data) =>{
+        if(err){
+            console.error('no se ha encontrado el archivo');
+        }else {
+            console.log(data.toString());
+        }  
     })
 }
 function escribir(ruta,contenido,callback){
@@ -20,8 +24,14 @@ function borrar(ruta, callback) {
 }
 
 function leertipo(ruta,callback){
-    callback(path.extname(ruta))
+     fs.readFile(ruta, (error) =>{
+        if(error){
+            console.error('no se ha encontrado el archivo');
+        } else {
+            callback(path.extname(ruta));
+        }
+    })
 }
-leertipo(__dirname + '/file.md',console.log);
+leer(__dirname + '/file.m',console.log);
 // escribir(__dirname + '/file.md', 'holas', console.log)
 // leer(__dirname + '/file.md', console.log)
